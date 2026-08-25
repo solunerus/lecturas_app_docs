6b9f09975caa00399a8aeb888d1e71593996f226
juliancitou <22690208@tecvalles.mx>
2026-08-25 16:02:14 -0600

    1- Lecturas

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Arquos OPR',
   tagline: '¡La mejor aplicacion para gestion y manejo de medidores de agua!',
-  favicon: 'img/logo_arquos_opr_vacio.png',
+  favicon: 'img/logo_arquos_crm.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -86,7 +86,7 @@ const config = {
         title: 'Arquos OPR',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo_arquos_opr_lleno.png',
+          src: 'img/logo_arquos_crm.png',
         },
         items: [
           {
